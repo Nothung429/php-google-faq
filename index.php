d9de82d8c65155afeb95dbda1760187c4b1d868b
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-23 14:31:32 +0200

    add files

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- Riscrivere questa pagina del sito google
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Googles Facs</title>
+    </head>
+
+    <body>
+        <header>
+            
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
